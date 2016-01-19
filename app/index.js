5e048740322a845dab767501eaa5561035d551f8
Lee <leeblazek@gmail.com>
2016-01-19 01:02:50 -0700

    toolbar working

--- app/index.js
+++ app/index.js
@@ -10,6 +10,6 @@ require('./page3/page3Module')(Angular);
 
 require('style!css!sass!../node_modules/angular-material/angular-material.scss')
 require('./styles/main.scss')
-const ngModule = Angular.module('myApp', [require('angular-ui-router'), 'oc.lazyLoad', 'page3App']);
+const ngModule = Angular.module('myApp', [require('angular-ui-router'), 'oc.lazyLoad', 'page3App', 'ngMaterial']);
 
 require('./config')(ngModule, Angular);
